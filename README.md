b10790807be384133a881c3c69a220319aa1b72e
Moinak <moinakbanerjee.98@gmail.com>
2019-03-27 20:33:10 +0530

    Creating new file

---
+++ README.md
@@ -0,0 +1,8 @@
+This Project is to create apis for CNN and KNN for credit card fraud detection.
+To run on localhost, download the project and perform the following steps
+1) open cmd
+2) pip install -r requirements.txt
+3) python3 application.py
+4) Send GET request to localhost:5000/accuracy/ to rest the accuracy of both the algorithms on test.csv
+5) Send POST request to localhost:5000/test/ with values for customer,age,gender,zipcodeOri,merchant,zipMerchant,category,amount
+
---
+++ application.py
@@ -0,0 +1,109 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Lambda
+from keras.layers import Embedding
+from keras.layers import Convolution1D,MaxPooling1D, Flatten
+from keras import backend as K
+from keras.layers import LSTM, GRU, SimpleRNN
+from keras.callbacks import CSVLogger
+from sklearn.ensemble import VotingClassifier
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger
+import h5py
+import pickle
+
+data = pd.read_csv("test.csv")
+X = data.drop("fraud", axis = 1)
+y = data["fraud"]
+y_num = y.values
+with open("knn.pkl", "rb") as f:
+	knn = pickle.load(f)
+lstm_output_size = 70
+def cnnmodel_test( a_knn, b_knn, c_knn, d_knn, e_knn, f_knn, g_knn, h_knn ):
+    arr_testing=pd.DataFrame({'customer':[a_knn],'age':[b_knn],'gender':[c_knn],'zipcodeOri':[d_knn],'merchant':[e_knn],'zipMerchant':[f_knn],'category':[g_knn],'amount':[h_knn]},columns=['customer','age','gender','zipcodeOri','merchant','zipMerchant','category','amount'])
+    test_features1 = np.reshape(arr_testing.values, (arr_testing.values.shape[0],arr_testing.values.shape[1],1))
+    cnn = Sequential()
+    cnn.add(Convolution1D(64, 3, activation="relu", input_shape= (8, 1)))
+    cnn.add(Convolution1D(64, 3, activation="relu"))
+    cnn.add(MaxPooling1D(pool_size=2))
+    cnn.add(Convolution1D(128, 3, activation="relu", padding = "same"))
+    cnn.add(Convolution1D(128, 3, activation="relu", padding = "same"))
+    cnn.add(MaxPooling1D(pool_size=2))
+    cnn.add(LSTM(lstm_output_size))
+    cnn.add(Dropout(0.1))
+    cnn.add(Dense(2, activation="softmax"))
+    cnn.load_weights("cnn_model.hdf5")
+    cnn.compile(loss="sparse_categorical_crossentropy", optimizer="SGD", metrics=['accuracy'])
+    print("Created model and loaded weights from file")
+    y_cnn = cnn.predict_classes(test_features1)
+    K.clear_session()
+    return str(y_cnn[0])
+def knnmodel_test( a_knn, b_knn, c_knn, d_knn, e_knn, f_knn, g_knn, h_knn ):
+    arr_testing=pd.DataFrame({'customer':[a_knn],'age':[b_knn],'gender':[c_knn],'zipcodeOri':[d_knn],'merchant':[e_knn],'zipMerchant':[f_knn],'category':[g_knn],'amount':[h_knn]},columns=['customer','age','gender','zipcodeOri','merchant','zipMerchant','category','amount'])
+    y_knn = knn.predict(arr_testing)
+    return str(y_knn[0])
+
+def knnmodel_accuracy():
+    y_knn = knn.predict(X)
+    return accuracy_score(y_num,y_knn)
+def cnnmodel_accuracy():
+    test_labels = np.array(y)
+    test_features = np.reshape(X.values, (X.values.shape[0],X.values.shape[1],1))
+    cnn = Sequential()
+    cnn.add(Convolution1D(64, 3, activation="relu", input_shape= (8, 1)))
+    cnn.add(Convolution1D(64, 3, activation="relu"))
+    cnn.add(MaxPooling1D(pool_size=2))
+    cnn.add(Convolution1D(128, 3, activation="relu", padding = "same"))
+    cnn.add(Convolution1D(128, 3, activation="relu", padding = "same"))
+    cnn.add(MaxPooling1D(pool_size=2))
+    cnn.add(LSTM(lstm_output_size))
+    cnn.add(Dropout(0.1))
+    cnn.add(Dense(2, activation="softmax"))
+    cnn.load_weights("cnn_model.hdf5")
+    cnn.compile(loss="sparse_categorical_crossentropy", optimizer="SGD", metrics=['accuracy'])
+    print("Created model and loaded weights from file")
+    y_cnn = cnn.predict_classes(test_features)
+    K.clear_session()
+    return accuracy_score(y_num,y_cnn)
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route("/accuracy/")
+def hel():
+    #knnmodel()
+    x = knnmodel_accuracy()
+    z = cnnmodel_accuracy()
+    y="Accuracy of the KNN model is: " + str(x) + "<br>\n Accuracy of the CNN Model is :" + str(z)
+    return str(y)
+
+@app.route("/test/", methods=['GET','POST'])
+def hello():
+    #knnmodel()
+    if request.method == 'POST':
+        a_knn = request.form['customer']
+        b_knn = request.form['age']
+        c_knn = request.form['gender']
+        d_knn = request.form['zipcodeOri']
+        e_knn = request.form['merchant']
+        f_knn = request.form['zipMerchant']         
+        g_knn = request.form['category']
+        h_knn = request.form['amount']
+        y="Result from KNN model: "+ str(knnmodel_test( a_knn, b_knn, c_knn, d_knn, e_knn, f_knn, g_knn, h_knn ))
+        y=y +"<br>Result from KNN model: "+ str(cnnmodel_test( a_knn, b_knn, c_knn, d_knn, e_knn, f_knn, g_knn, h_knn ))
+        return y
+    if request.method == 'GET':
+    	return "Error Wrong Request"
+    return str(-1)
+
+
+if __name__=='__main__':
+	app.run()
